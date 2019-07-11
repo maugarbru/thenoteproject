1a4eb5fc41d0b52e38cd174c863dfd5efcf0dc80
maugarbru <mauriciogarzonb@gmail.com>
2019-07-11 16:15:24 -0500

    Agrega Bootstrap-Vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,31 +1,23 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <b-navbar toggleable="lg" type="light" variant="warning">
+      <b-container>
+        <b-navbar-brand :to="{name: 'home'}">LaNota.com</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item class="text-center" :to="{name: 'home'}" exact>Inicio</b-nav-item>
+            <b-nav-item class="text-center" :to="{name: 'login'}">Login</b-nav-item>
+            <b-nav-item class="text-center" :to="{name: 'about'}">Acerca</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+
+    <b-container>
+      <router-view />
+    </b-container>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,16 +1,12 @@
 <template>
-  <div class="about">
+  <div class="login">
     <div class="mt-5">
       <div>
         <b-card-group deck class="text-center">
           <b-card
-            header="Desarrollado por"
-            img-src="https://bit.ly/2Mk2OJU"
-            img-alt="Image"
-            img-top
-            tag="article"
+            header="Ingrese sus datos"
             style="max-width: 20rem; margin: auto;"
-            title="Mauricio Garzón Brunal"
+            title="Iniciar sesión"
           >
             <b-card-text>Desarrollado usando Vue.js y Node.js</b-card-text>
             <b-button target="blank" href="https://github.com/maugarbru/cd-prueba" variant="dark">Repositorio en GitHub</b-button>
@@ -23,6 +19,6 @@
 
 <script>
 export default {
-  name: "About"
+  name: "Login"
 };
 </script>
